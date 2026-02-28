8bdb2381b6be1ba82a60cc6c5a8167ef79b31756
miller-salins <miller.salins@gmail.com>
2026-02-27 23:45:11 -0500

    Fix: added cleaning command for different types of date formats

--- scripts/02_data_cleaning_process.sql
+++ scripts/02_data_cleaning_process.sql
@@ -2,14 +2,20 @@
 -- Standarize date formats and cleaning blank spaces
 
 -- Cleaning for raw_payment_table
+
+-- you may need to use "SET SQL_SAFE_UPDATES = 0;"" before the next lines
 UPDATE raw_payment_audit
-SET payment_date = STR_TO_DATE(payment_date, '%Y-%M-%D'),
-    payment_status = TRIM(UPPER(payment_status)),
-    source_system = TRIM(source_system)
+SET payment_date = CASE
+    WHEN payment_date LIKE '%/%' THEN STR_TO_DATE(payment_date, '%d/%m/%Y')
+    WHEN payment_date LIKE '%-%' THEN STR_TO_DATE(payment_date, '%Y-%m-%d')
+    ELSE payment_date
+END,
+payment_status = TRIM(UPPER(payment_status)),
+source_system = TRIM(source_system);
 
 ALTER TABLE raw_payment_audit
 MODIFY COLUMN payment_date DATE,
 MODIFY COLUMN amount_billed DECIMAL(10,2),
 MODIFY COLUMN amount_paid DECIMAL(10,2),
 MODIFY COLUMN payment_status VARCHAR(50),
-MODIFY COLUMN source_system VARCHAR(50),
+MODIFY COLUMN source_system VARCHAR(50);
