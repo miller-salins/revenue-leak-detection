b76e04ec229526d5e144c0c5c1194eaa526cf22f
miller-salins <miller.salins@gmail.com>
2026-03-04 19:42:10 -0500

    Feat: created final standard plan table

--- scripts/04_production_schema.sql
+++ scripts/04_production_schema.sql
@@ -1,5 +1,6 @@
 -- Final tables to migrate the clean data.
 
+-- Customer Table
 CREATE TABLE customer(
     -- 1. Internal Key (For secure relationships within the DB)
     db_customer_id INTEGER PRIMARY KEY AUTO_INCREMENT, 
@@ -21,7 +22,33 @@ CREATE TABLE customer(
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
 -- Performance Optimization Indexes
 CREATE INDEX idx_customer_source_id ON customer(source_customer_id);
 CREATE INDEX idx_customer_email ON customer(email);
+
+
+-- PLAN TABLE
+CREATE TABLE plan (
+    -- 1. Internal Key
+    db_plan_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        -- 2. External Key
+    source_plan_id VARCHAR(100) NOT NULL,
+    -- 3. Core Business Data
+    plan_name VARCHAR(255),
+    price DECIMAL(18,8), 
+    currency VARCHAR(10),
+    billing_cycle VARCHAR(50),
+    is_active TINYINT DEFAULT 1,
+    -- 4. Scalability
+    metadata JSON,
+    -- 5. Lineage and Audit
+    batch_id VARCHAR(50),
+    source_system VARCHAR(50),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Performance Optimization Indexes
+CREATE INDEX idx_plan_source_id ON plan(source_plan_id);
+
+
