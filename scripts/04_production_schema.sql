6575d325e1173af23ea9fc925cf1558723f20c67
miller-salins <miller.salins@gmail.com>
2026-03-04 19:20:27 -0500

    setting up production schema with universally structured final tables. I added a metadata column (set to receive JSON files) to include unforeseen data that can be used by a bussiness

---
+++ scripts/04_production_schema.sql
@@ -0,0 +1,25 @@
+-- Final tables to migrate the clean data.
+
+CREATE TABLE customer(
+    -- 1. Internal Key (For secure relationships within the DB)
+    db_customer_id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+    -- 2. External Key (The ID coming from Stripe, Apple, etc.)
+    source_customer_id VARCHAR(100) NOT NULL, 
+    -- 3. Core Business (Standard business columns)
+    customer_name VARCHAR(255), 
+    email VARCHAR(255), 
+    phone_number VARCHAR(50), 
+    signup_date DATE, 
+    plan_type VARCHAR(50), 
+    customer_status VARCHAR(50), 
+    is_active TINYINT DEFAULT 1, 
+    -- 4. (Unforeseen extra data from external sources)
+    metadata JSON, 
+    -- 5. Lineage and Audit
+    batch_id VARCHAR(50), 
+    source_system VARCHAR(50), 
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
